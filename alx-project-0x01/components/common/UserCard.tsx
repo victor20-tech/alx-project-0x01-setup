4104ed859ad75642f11b10c157136d19a6ed6f1d
Victor Adomako <victoradomako10@gmail.com>
2025-10-03 19:14:30 -0400

    Update UserCard.tsx

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,19 +1,19 @@
 import { UserProps } from '@/interfaces';
 import React from 'react';
 
-const UserCard: React.FC<UserCardProps> = ({ user }) => {
+const UserCard: React.FC<UserProps> = ({ name, username, email, website }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-6 m-4 border border-gray-200 w-full max-w-sm">
-      <h2 className="text-2xl font-bold text-blue-700 mb-2">{user.name}</h2>
-      <p className="text-gray-600 italic">@{user.username}</p>
+      <h2 className="text-2xl font-bold text-blue-700 mb-2">{name}</h2>
+      <p className="text-gray-600 italic">@{username}</p>
       <div className="mt-4 border-t pt-4">
         <p className="text-sm text-gray-800">
-          <strong>Email:</strong> {user.email}
+          <strong>Email:</strong> {email}
         </p>
         <p className="text-sm text-gray-800">
           <strong>Website:</strong>{" "}
-          <a href={`http://${user.website}`} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
-            {user.website}
+          <a href={`http://${website}`} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+            {website}
           </a>
         </p>
       </div>
