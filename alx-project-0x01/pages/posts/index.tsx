132399068c85c227ec89f387d028f9a8783eb053
Victor Adomako <victoradomako10@gmail.com>
2025-11-10 20:36:21 +0000

    Update index.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,27 +1,16 @@
 import PostCard from "@/components/common/PostCard";
-import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostData, PostProps } from "@/interfaces";
-import { useState } from "react";
+import { PostProps } from "@/interfaces";
 
-const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [post, setPost] = useState<PostData | null>(null);
-
-
-  const handleAddPost = (newPost: PostData) => {
-    setPost({ ...newPost, id: posts.length + 1 });
-  };
-
-
+const Posts: React.FC<PostProps[]> = ({ posts }) => {
+  console.log(posts)
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
-          <h1 className=" text-2xl font-semibold">Post Content</h1>
-          <button onClick={() => setModalOpen(true)}
-            className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        <h1 className=" text-2xl font-semibold">Post Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
         </div>
         <div className="grid grid-cols-3 gap-2 ">
           {
@@ -31,10 +20,6 @@ const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
           }
         </div>
       </main>
-
-      {isModalOpen && (
-        <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
-      )}
     </div>
   )
 }
